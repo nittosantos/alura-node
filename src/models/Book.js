e1293495c742843375ba8edd4731316efa395e2e
Nitto <nitto_kun@hotmail.com>
2023-06-14 10:26:45 -0300

    refactor: :art:

--- src/models/Book.js
+++ src/models/Book.js
@@ -6,7 +6,7 @@ const bookSchema = new mongoose.Schema(
     title: { type: 'string', required: [true, 'O campo Titulo do livro é obrigatório']},
     author: { type: mongoose.Schema.Types.ObjectId, ref:'authors', required: [true, 'Autor(a) é obrigatório ']},
     publish: { type: 'string', required: [true, 'Editora obrigatória']},
-    numberOfPages: { type: Number}
+    numberOfPages: { type: Number, min: [1, 'O livro deve ter no mínimo 1 página'], max: [5000, 'O livro deve ter no máximo 5000 páginas']}
   }
 );
 
